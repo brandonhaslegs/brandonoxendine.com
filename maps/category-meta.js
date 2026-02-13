8e746cf142318a5fe4ab093e6e859e044dd3c238
Brandon Oxendine <brandon.oxendine@gmail.com>
2026-02-13 18:43:28 +0100

    reformat the app to be organized, import data with json files

---
+++ maps/category-meta.js
@@ -0,0 +1,74 @@
+window.CATEGORY_META = {
+  "art": {
+    "label": "Arts",
+    "emoji": "🖼️"
+  },
+  "bakery": {
+    "label": "Bakeries",
+    "emoji": "🥐"
+  },
+  "bar": {
+    "label": "Bars",
+    "emoji": "🍺"
+  },
+  "cafe": {
+    "label": "Cafes",
+    "emoji": "☕"
+  },
+  "cafes & bakeries": {
+    "label": "Cafes & Bakeries",
+    "emoji": "🥐"
+  },
+  "club": {
+    "label": "Clubs",
+    "emoji": "🎶"
+  },
+  "experience": {
+    "label": "Experiences",
+    "emoji": "🎟️"
+  },
+  "flea market": {
+    "label": "Flea Markets",
+    "emoji": "🏷️"
+  },
+  "hotel": {
+    "label": "Hotels",
+    "emoji": "🛏️"
+  },
+  "music venue": {
+    "label": "Music Venues",
+    "emoji": "🎤"
+  },
+  "nail salon": {
+    "label": "Nail Salons",
+    "emoji": "💅"
+  },
+  "nature": {
+    "label": "Nature",
+    "emoji": "🏔️"
+  },
+  "other": {
+    "label": "Other Places",
+    "emoji": "📍"
+  },
+  "park": {
+    "label": "Parks",
+    "emoji": "🌳"
+  },
+  "restaurant": {
+    "label": "Restaurants",
+    "emoji": "🍽️"
+  },
+  "shop": {
+    "label": "Shops",
+    "emoji": "🛍️"
+  },
+  "tattoo": {
+    "label": "Tattoo",
+    "emoji": "🖋️"
+  },
+  "vintage": {
+    "label": "Vintage Shops",
+    "emoji": "🧥"
+  }
+};
---
+++ maps/city-meta.js
@@ -0,0 +1,586 @@
+window.CITY_META = {
+  "Adelaide": {
+    "emoji": "📍",
+    "country": "Australia"
+  },
+  "Amsterdam": {
+    "emoji": "🚲",
+    "country": "Netherlands"
+  },
+  "Antwerpen": {
+    "emoji": "📍",
+    "country": "Belgium"
+  },
+  "Argentina": {
+    "emoji": "🇦🇷",
+    "country": "Argentina"
+  },
+  "Athens": {
+    "emoji": "🏛️",
+    "country": "Greece"
+  },
+  "Austin": {
+    "emoji": "🤠",
+    "country": "United States"
+  },
+  "Av. Brasília": {
+    "emoji": "📍",
+    "country": "Portugal"
+  },
+  "Balearic Islands": {
+    "emoji": "🏝️",
+    "country": "Spain"
+  },
+  "Bangkok": {
+    "emoji": "🛺",
+    "country": "Thailand"
+  },
+  "Barcelona": {
+    "emoji": "🐂",
+    "country": "Spain"
+  },
+  "Basel": {
+    "emoji": "🧀",
+    "country": "Switzerland"
+  },
+  "Bay Area": {
+    "emoji": "🌉",
+    "country": "United States"
+  },
+  "Berlin": {
+    "emoji": "🐻",
+    "country": "Germany"
+  },
+  "Bordeaux": {
+    "emoji": "🍷",
+    "country": "France"
+  },
+  "Brazil": {
+    "emoji": "🇧🇷",
+    "country": "Brazil"
+  },
+  "Brussels": {
+    "emoji": "🍟",
+    "country": "Belgium"
+  },
+  "Budapest": {
+    "emoji": "🛁",
+    "country": "Hungary"
+  },
+  "Calvi": {
+    "emoji": "📍",
+    "country": "France"
+  },
+  "Charleston": {
+    "emoji": "⚜️",
+    "country": "United States"
+  },
+  "Charlotte": {
+    "emoji": "🐝",
+    "country": "United States"
+  },
+  "Cheyenne": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Chicago": {
+    "emoji": "🌭",
+    "country": "United States"
+  },
+  "Cleebronn": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "Cologne": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "Columbia": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Copenhagen": {
+    "emoji": "🚲",
+    "country": "Denmark"
+  },
+  "Corfu": {
+    "emoji": "📍",
+    "country": "Greece"
+  },
+  "Den Haag": {
+    "emoji": "📍",
+    "country": "Netherlands"
+  },
+  "Denver": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Des Moines": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Detroit": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Dhërmi": {
+    "emoji": "📍",
+    "country": "Albania"
+  },
+  "Dresden": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "Duisburg": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "Durrës": {
+    "emoji": "📍",
+    "country": "Albania"
+  },
+  "Düsseldorf": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "Edinburgh": {
+    "emoji": "📍",
+    "country": "United Kingdom"
+  },
+  "Edisto Island": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Elsdorf": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "Essen": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "Fehmarn": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "Finland": {
+    "emoji": "📍",
+    "country": "Finland"
+  },
+  "Florence": {
+    "emoji": "📍",
+    "country": "Italy"
+  },
+  "Fort Mill": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Framersheim": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "Frankfurt": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "Gipuzkoa": {
+    "emoji": "📍",
+    "country": "Spain"
+  },
+  "Girona": {
+    "emoji": "📍",
+    "country": "Spain"
+  },
+  "Gjirokastër": {
+    "emoji": "📍",
+    "country": "Albania"
+  },
+  "Glasgow": {
+    "emoji": "📍",
+    "country": "United Kingdom"
+  },
+  "Grad Dubrovnik": {
+    "emoji": "📍",
+    "country": "Croatia"
+  },
+  "Greenville": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Grindelwald": {
+    "emoji": "📍",
+    "country": "Switzerland"
+  },
+  "Guatemala": {
+    "emoji": "📍",
+    "country": "Guatemala"
+  },
+  "Hamburg": {
+    "emoji": "⚓",
+    "country": "Germany"
+  },
+  "Ho Chi Minh City": {
+    "emoji": "📍",
+    "country": "Vietnam"
+  },
+  "Illes Balears": {
+    "emoji": "📍",
+    "country": "Spain"
+  },
+  "India": {
+    "emoji": "📍",
+    "country": "India"
+  },
+  "Istanbul": {
+    "emoji": "🧿",
+    "country": "Turkey"
+  },
+  "Iwakuni": {
+    "emoji": "📍",
+    "country": "Japan"
+  },
+  "Kraków": {
+    "emoji": "🐉",
+    "country": "Poland"
+  },
+  "Krujë": {
+    "emoji": "📍",
+    "country": "Albania"
+  },
+  "Kyiv": {
+    "emoji": "🇺🇦",
+    "country": "Ukraine"
+  },
+  "Laramie": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Las Palmas": {
+    "emoji": "🌴",
+    "country": "Spain"
+  },
+  "Las Vegas": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Leipzig": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "Lingen (Ems)": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "Lisbon": {
+    "emoji": "🌊",
+    "country": "Portugal"
+  },
+  "London": {
+    "emoji": "🚌",
+    "country": "United Kingdom"
+  },
+  "Los Angeles": {
+    "emoji": "🎬",
+    "country": "United States"
+  },
+  "Madison": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Madrid": {
+    "emoji": "🌞",
+    "country": "Spain"
+  },
+  "Mainz": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "Malta": {
+    "emoji": "📍",
+    "country": "Malta"
+  },
+  "Manchester": {
+    "emoji": "📍",
+    "country": "United Kingdom"
+  },
+  "Manila": {
+    "emoji": "📍",
+    "country": "Philippines"
+  },
+  "Marrakesh": {
+    "emoji": "📍",
+    "country": "Morocco"
+  },
+  "Marseille": {
+    "emoji": "⚓",
+    "country": "France"
+  },
+  "Metzingen": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "Mexico City": {
+    "emoji": "🌮",
+    "country": "Mexico"
+  },
+  "Miami": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Milano MI": {
+    "emoji": "📍",
+    "country": "Italy"
+  },
+  "Minneapolis": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Montreal": {
+    "emoji": "📍",
+    "country": "Canada"
+  },
+  "Montréal": {
+    "emoji": "📍",
+    "country": "Canada"
+  },
+  "Munich": {
+    "emoji": "🍺",
+    "country": "Germany"
+  },
+  "NYC": {
+    "emoji": "🗽",
+    "country": "United States"
+  },
+  "Nakagyo Ward": {
+    "emoji": "📍",
+    "country": "Japan"
+  },
+  "Nashville": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Neubrandenburg": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "New Orleans": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Nice": {
+    "emoji": "📍",
+    "country": "France"
+  },
+  "Nienhagen": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "Noordwijk": {
+    "emoji": "📍",
+    "country": "Netherlands"
+  },
+  "Norway": {
+    "emoji": "📍",
+    "country": "Norway"
+  },
+  "Nürnberg": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "Orlando": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Other": {
+    "emoji": "📍",
+    "country": "Unknown"
+  },
+  "Palm Springs": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Palma Mallorca": {
+    "emoji": "📍",
+    "country": "Spain"
+  },
+  "Paris": {
+    "emoji": "🥐",
+    "country": "France"
+  },
+  "Peru": {
+    "emoji": "📍",
+    "country": "Peru"
+  },
+  "Philadelphia": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Phoenix": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Pocahontas": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Portland ME": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Portland OR": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Porto": {
+    "emoji": "🍷",
+    "country": "Portugal"
+  },
+  "Prague": {
+    "emoji": "⛪",
+    "country": "Czech Republic"
+  },
+  "Prishtina": {
+    "emoji": "🦅",
+    "country": "Kosovo"
+  },
+  "Reinhardtsdorf-Schöna": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "Riga": {
+    "emoji": "🌲",
+    "country": "Latvia"
+  },
+  "Rock Hill": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Rosenthal-Bielatal": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "Rust": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "San Antonio": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "San Diego": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Sassnitz": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "Savannah": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Seattle": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Seoul": {
+    "emoji": "🎧",
+    "country": "South Korea"
+  },
+  "Singapore": {
+    "emoji": "📍",
+    "country": "Singapore"
+  },
+  "Sofia": {
+    "emoji": "📍",
+    "country": "Bulgaria"
+  },
+  "South Africa": {
+    "emoji": "📍",
+    "country": "South Africa"
+  },
+  "St. Louis": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Stockholm": {
+    "emoji": "🛶",
+    "country": "Sweden"
+  },
+  "Sydney": {
+    "emoji": "📍",
+    "country": "Australia"
+  },
+  "Tbilisi": {
+    "emoji": "📍",
+    "country": "Georgia"
+  },
+  "Tiranë": {
+    "emoji": "📍",
+    "country": "Albania"
+  },
+  "Tokyo": {
+    "emoji": "🗼",
+    "country": "Japan"
+  },
+  "Toronto": {
+    "emoji": "🍁",
+    "country": "Canada"
+  },
+  "Uccle": {
+    "emoji": "📍",
+    "country": "Belgium"
+  },
+  "United Arab Emirates": {
+    "emoji": "📍",
+    "country": "United Arab Emirates"
+  },
+  "Vancouver": {
+    "emoji": "📍",
+    "country": "Canada"
+  },
+  "Vilnius": {
+    "emoji": "📍",
+    "country": "Lithuania"
+  },
+  "Vlorë": {
+    "emoji": "📍",
+    "country": "Albania"
+  },
+  "Warsaw": {
+    "emoji": "🧜",
+    "country": "Poland"
+  },
+  "Washington": {
+    "emoji": "📍",
+    "country": "United States"
+  },
+  "Wien": {
+    "emoji": "🎻",
+    "country": "Austria"
+  },
+  "Wiesbaden": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "Wrocław": {
+    "emoji": "📍",
+    "country": "Poland"
+  },
+  "Wustermark": {
+    "emoji": "📍",
+    "country": "Germany"
+  },
+  "Zermatt": {
+    "emoji": "📍",
+    "country": "Switzerland"
+  },
+  "Zürich": {
+    "emoji": "📍",
+    "country": "Switzerland"
+  },
+  "Δήμος Μήλου": {
+    "emoji": "📍",
+    "country": "Greece"
+  }
+};
